e5f14e71f07ca828b5e311519bbc6e3a18d296cd
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-11 21:06:36 +0100

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+function playRound(playerSelection, computerSelection) {
+    
+  }
+   
+ function getComputerChoice() {
+    var gameContent = ['Rock', 'Paper', 'Scissor'];
+
+   
+
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
