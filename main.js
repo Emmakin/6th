661a81adcc7e310e985f5a229cf68ac612fa7b6d
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-12 01:05:52 +0100

    made the javascript active agin

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
- function getComputerChoice() {
-    var gameContent = ['Rock', 'Paper', 'Scissor'];
-   function random(){
-    return gameContent[Math.floor(Math.random() * gameContent.length)]
-   }
-   random();
-  };
-   
+var gameContent = ['rock', 'paper', 'scissor'];
+// const playerSelection = "rock";
 
-  const playerSelection = "rock";
+
+//  function  getUserPick(){
+//     let playerSelection = document.querySelector("#userPick").value;
+//     return playerSelection;
+//  } 
+//  console.log(getUserPick());
+
+let playerSelection = prompt();
+console.log(playerSelection.toLowerCase());
+
+ function getComputerChoice() {
+    return gameContent[Math.floor(Math.random() * gameContent.length)] 
+  };
+ 
+   function playRound(){
+      
+      if (computerSelection === playerSelection){
+         console.log("Draw");
+      } else if (computerSelection == 'scissor' && playerSelection == 'rock'){
+      console.log("You won! rock beats scissor")
+      } else if (computerSelection == 'paper' && playerSelection == 'rock'){
+         console.log("You lose! paper beats rock")
+      } else {
+         console.log("try again")
+      }
+   }; 
+
+
+
+ 
   const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playRound());
