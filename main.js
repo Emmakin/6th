5ab44e6ac23ddea74d76553f80eee1e5014750d2
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-12 23:07:27 +0100

    update the entire function  for the game to run

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 var gameContent = ['rock', 'paper', 'scissor'];
-// const playerSelection = "rock";
-
-
-//  function  getUserPick(){
-//     let playerSelection = document.querySelector("#userPick").value;
-//     return playerSelection;
-//  } 
-//  console.log(getUserPick());
-
-let playerSelection = prompt();
-console.log(playerSelection.toLowerCase());
 
  function getComputerChoice() {
     return gameContent[Math.floor(Math.random() * gameContent.length)] 
@@ -19,18 +8,39 @@ console.log(playerSelection.toLowerCase());
       
       if (computerSelection === playerSelection){
          console.log("Draw");
+         let i = 0;
+         let j = 0; 
       } else if (computerSelection == 'scissor' && playerSelection == 'rock'){
-      console.log("You won! rock beats scissor")
+         console.log("You won! rock beats scissor")
       } else if (computerSelection == 'paper' && playerSelection == 'rock'){
          console.log("You lose! paper beats rock")
-      } else {
+      } else if (computerSelection == 'rock' && playerSelection == 'scissor'){
+         console.log("You lose! rock beats scissor")
+      }else if (computerSelection == 'paper' && playerSelection == 'scissor'){
+         console.log("You win! scissor beats paper")
+      }else if (computerSelection == 'rock' && playerSelection == 'paper'){
+         console.log("You win! paper beats rock")
+      }else if (computerSelection == 'scissor' && playerSelection == 'paper'){
+         console.log("You lose! paper beats rock")
+      }
+      else {
          console.log("try again")
       }
    }; 
+
+   // for (let i = 0, j = 0; 
+   //          i <= 5 ||  j <= 5; 
+   //          i++, j++) {
+   //             console.log(i,j)
+      
+   // }
 
 
 
  
   const computerSelection = getComputerChoice();
-  console.log(computerSelection);
+  console.log( `Computer picked ${computerSelection}`);
+  let playerSelection = prompt('rock, paper or scissors');
+  console.log(`You picked ${playerSelection.toLowerCase()}`);
+
   console.log(playRound());
