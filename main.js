a6698be868cf7bd1dd96387619e49aace320bfb5
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-09-13 23:36:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var gameContent = ['rock', 'paper', 'scissor'];
-let i = 0;
-let j = 0;
 
  function getComputerChoice() {
     return gameContent[Math.floor(Math.random() * gameContent.length)] 
@@ -12,41 +10,24 @@ let j = 0;
          console.log("Draw");
       } else if (computerSelection == 'scissor' && playerSelection == 'rock'){
          console.log("You won! rock beats scissor")
-         j ++;
       } else if (computerSelection == 'paper' && playerSelection == 'rock'){
          console.log("You lose! paper beats rock")
-         i++;
       } else if (computerSelection == 'rock' && playerSelection == 'scissor'){
          console.log("You lose! rock beats scissor")
-         i++;
       }else if (computerSelection == 'paper' && playerSelection == 'scissor'){
          console.log("You win! scissor beats paper")
-         j++;
       }else if (computerSelection == 'rock' && playerSelection == 'paper'){
          console.log("You win! paper beats rock")
-         j++;
       }else if (computerSelection == 'scissor' && playerSelection == 'paper'){
          console.log("You lose! paper beats rock")
-         i++;
       }
       else {
          console.log("try again")
       }
    }; 
 
-   
-            
-   function cancelGame(){
-
-      if (i <= 5 ||  j <= 5){
-      console.log(i,j)}
-   }; 
               
       
-
-
-
-
  
   const computerSelection = getComputerChoice();
   console.log( `Computer picked ${computerSelection}`);
